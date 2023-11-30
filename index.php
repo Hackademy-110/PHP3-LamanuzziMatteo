bc5da110739bbe4ff1a5bb5237b021aa5c6112ed
Matteo <lamanuzzi.matteo@gmail.com>
2023-11-30 15:00:15 +0100

    manca es3

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+class Person{
+    public $nome;
+    public $cognome;
+    public static $counter=0;
+    public function __construct($name,$surname){
+        $this->nome=$name;
+        $this->cognome=$surname;
+        self::$counter++;
+    }
+}
+$Paolo= new Person('Paolo','Rossi');
+class Student extends Person{
+    public $anno;
+    public $sezione;
+    public function __construct($name,$surname,$year,$section){
+        parent::__construct($name,$surname);
+        $this->anno=$year;
+        $this->sezione=$section;
+    }
+}
+$Luigi= new Student('Luigi','Paoletti','Terzo','G');
+
+class Teacher extends Person{
+    public $materia;
+    public $tipoContratto;
+    public function __construct($name,$surname,$topic,$contract){
+        parent::__construct($name,$surname);
+        $this->materia=$topic;
+        $this->tipoContratto=$contract;
+    }
+}
+$minigheddu= new Teacher('Franca','Minigheddu','Storia','Indeterminata');
+var_dump($minigheddu->counter);
---
+++ zoo.php
@@ -0,0 +1,31 @@
+<?php
+class Animal{
+    public $name;
+    public $species;
+    public $diet;
+    public function __construct($name,$species,$diet){
+        $this->name=$name;
+        $this->species=$species;
+        $this->diet=$diet;
+    }
+}
+class Bear extends Animal{
+    public $habitat;
+    public $lethargy;
+    public function __construct($name,$species,$diet,$habitat,$lethargy){
+        parent::__construct($name,$species,$diet);
+        $this->habitat=$habitat;
+        $this->lethargy=$lethargy;
+    }
+}
+$marco= new Bear('Marco','Orso','Onnivoro','foresta','letargo stagionale');
+class Wolf extends Animal{
+    public $subspecies;
+    public $group;
+    public function __construct($name,$species,$diet,$subspecies,$group){
+        parent::__construct($name,$species,$diet);
+        $this->subspecies=$subspecies;
+        $this->group=$group;
+    }
+}
+$lucio= new Wolf('Lucio','lupo','carnivoro','Canis lupus italicus','Branco di Zolta');
